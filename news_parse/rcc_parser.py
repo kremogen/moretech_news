5a850f07b054182c21894dba59a3b8e50bd2784b
noble6 <bitcoineazy@gmail.com>
2022-10-08 16:57:25 +0300

    rcc parser

--- news_parse/rcc_parser.py
+++ news_parse/rcc_parser.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import pandas as pd
+
+rcc_df = pd.DataFrame()
+
+rcc_df["Article"] = [""]
+rcc_df["Body"] = [""]
+rcc_df["Date"] = [""]
 
 URL = 'https://www.michael-smirnov.ru/Get_RSS.php?pRSSurl=https://www.banki.ru/xml/news.rss'
 URL_NEW = 'https://www.michael-smirnov.ru/Get_RSS.php?pRSSurl=https%3A%2F%2Fvc.ru%2Ffeed'
@@ -9,10 +16,41 @@ response = requests.get(URL)
 
 soup = BeautifulSoup(response.text, 'lxml')
 
+def total_addition():
+    date_get = soup.find_all(class_='pubd nowr')
+    body_get = soup.find_all('tr')
+    ahref_news = soup.find_all(class_='ahrefnews')
+    for i in range(len(ahref_news)):
+        if i > 1:
+            line_title = ahref_news[i]
+            line_body = body_get[i]
+            line_date = date_get[i]
+            rcc_df.loc[i] = [line_title.get_text(), line_body.get_text(), line_date.get_text()]
+
+    # # убирать первые 2 строки
+    # for line in soup.find_all('tr'):
+    #     # print(type(line.get_text()))
+    #     if line.get_text().startswith((' ', '\t')):
+    #         print('1')
+    #     else:
+    #         print(line.get_text())
+    #
+    # # убирать самую первую дату
+    # for line in soup.find_all(class_='pubd nowr'):
+    #     print(line.get_text())
+
 
 def title_normalizer():
-    for line in soup.find_all(class_='ahrefnews'):
-        print(line.get_text())
+    ahref_news = soup.find_all(class_='ahrefnews')
+    for i in range(len(ahref_news)):
+        if i > 1:
+            line = ahref_news[i]
+            rcc_df.loc[i] = line.get_text()
+
+    # for line in soup.find_all(class_='ahrefnews'):
+    #     print(line.get_text())
+    #     line.get_text()
+    #     rcc_df.loc[]
 
 
 def text_normalizer():
@@ -32,10 +70,8 @@ def date_checker():
 
 
 def get_info():
-    title_normalizer()
-    text_normalizer()
-    date_checker()
-    # print(response.text)
+    total_addition()
 
 
 get_info()
+print(rcc_df)
