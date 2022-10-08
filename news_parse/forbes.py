5c87f9ceb0b004a96bdf240a700da0087ebe664e
hteppl <hteppl.dev@gmail.com>
2022-10-08 03:41:05 +0300

    Update forbes.py

--- news_parse/forbes.py
+++ news_parse/forbes.py
@@ -5,46 +5,48 @@ API_BASE = 'https://www.forbes.ru/'
 API_LINK = 'https://api.forbes.ru/api/pub/lists/biznes'
 
 
-def get_last_news(offset):
+def get_last_news_data(offset):
     data = {
         'list[limit]': 11,
         'list[offset]': offset
     }
-    print(offset)
-    response = requests.get(API_LINK, params=data)
-    result = []
 
+    response = requests.get(API_LINK, params=data)
     try:
         res = response.json()
-        articles = res['articles']
-
-        for i in range(len(articles)):
-            result.append(API_BASE + articles[i]['data']['url_alias'])
-
+        return res['articles']
     except Exception as e:
         print(e)
 
-    return result
+    return None
 
 
 def get_news_html(url: str):
     resource = requests.get(url)
-    text_normalizer(resource.text)
-    print(resource.text)
+    print(text_normalizer(resource.text))
 
 
-def text_normalizer(text):
+def text_normalizer(text) -> list:
     soup = BeautifulSoup(text, 'lxml')
     quotes = soup.find_all(itemprop='articleBody')
-    print(quotes)
+    return quotes
 
+
+def get_news_link(url_alias: str):
+    return API_BASE + url_alias
+
+
+# между тегами с классом _3Ywvx находится контент новости
+# data-hid="og:title" property="og:title" content= - заголовок новости
 
 if __name__ == '__main__':
     offset = 0
-    for i in range(10):
-        last_news = get_last_news(offset)
+    for i in range(2):
+        last_news_data = get_last_news_data(offset)
         offset += 11
-        for j in range(len(last_news) - 1):
-            get_news_html(last_news[j])
-    # между тегами с классом _3Ywvx находится контент новости
-    # data-hid="og:title" property="og:title" content= - заголовок новости
+        for j in range(len(last_news_data) - 1):
+            news_link = get_news_link(last_news_data[j]['data']['url_alias'])
+            print(news_link)
+            print('date: ' + str(last_news_data[j]['data']['photo']['timestamp']))
+            get_news_html(news_link)
+            print('\n')
