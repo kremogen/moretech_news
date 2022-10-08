d3a53324c6ea31ba88b0548c7a96f92a21c48139
hteppl <hteppl.dev@gmail.com>
2022-10-08 19:44:04 +0300

    Update tg_bot.py

--- telegram/tg_bot.py
+++ telegram/tg_bot.py
@@ -4,19 +4,6 @@ from telebot import types
 
 bot = telebot.TeleBot('5684608047:AAFa-ki6eE395VkatiMJMPIsHItLpJHLdjE')
 
-try:
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM users')
-    conn.close()
-    print('База данных найдена, начинаю работу!')
-except Exception:
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE users (id integer, role text, keys text)')
-    conn.commit()
-    conn.close()
-    print('База данных не найдена, создаю и начинаю работу!')
 
 @bot.message_handler(content_types=['text'])
 def start(message):
@@ -24,35 +11,43 @@ def start(message):
         try:
             conn = sqlite3.connect("users.db")
             cursor = conn.cursor()
-            cursor.execute('SELECT * FROM users WHERE id = %s' % message.from_user.id)
+            cursor.execute('SELECT `id` FROM users WHERE id = ?', (message.from_user.id,))
+            id = cursor.fetchone()
             cursor.close()
             conn.close()
-            keyboard = types.InlineKeyboardMarkup()
-            key_buh = types.InlineKeyboardButton(text='Бухгалтер', callback_data='buh')
-            keyboard.add(key_buh)
-            key_dir = types.InlineKeyboardButton(text='Директор', callback_data='dir')
-            keyboard.add(key_dir)
-            conn = sqlite3.connect("users.db")
-            cursor = conn.cursor()
-            [role], = cursor.execute('SELECT role FROM users WHERE id = %s' % message.from_user.id)
-            conn.close()
-            if str(role).strip():
-                key_other = types.InlineKeyboardButton(text=str(role), callback_data=str(role))
-                keyboard.add(key_other)
+
+            if id is None:
+                create_user_profile(message.from_user.id)
             else:
-                key_other = types.InlineKeyboardButton(text='Другая роль', callback_data='another')
-                keyboard.add(key_other)
+                pass
+
+            # keyboard = types.InlineKeyboardMarkup()
+            # key_buh = types.InlineKeyboardButton(text='Бухгалтер', callback_data='buh')
+            # keyboard.add(key_buh)
+            # key_dir = types.InlineKeyboardButton(text='Директор', callback_data='dir')
+            # keyboard.add(key_dir)
+            #
+            # conn2 = sqlite3.connect("users.db")
+            # cursor2 = conn2.cursor()
+            # cursor2.execute('SELECT role FROM users WHERE id = ?', (message.from_user.id,))
+            # role = cursor2.fetchone()
+            # cursor2.close()
+            # conn2.close()
+            #
+            # if role is not None:
+            #     key_other = types.InlineKeyboardButton(text=str(role), callback_data=str(role))
+            #     keyboard.add(key_other)
+            # else:
+
+            key_other = types.InlineKeyboardButton(text='Другая роль', callback_data='another')
+            keyboard.add(key_other)
             bot.send_message(message.from_user.id,
-                            'Привет, я бот MoreTechNews\n\nЯ подберу для тебя самые релевантные новости исходя из выбранной роли\n\nУкажи, какая роль тебе подходит больше всего',
-                            reply_markup=keyboard)
+                             'Привет, я бот MoreTechNews\n\nЯ подберу для тебя самые релевантные новости исходя из '
+                             'выбранной роли\n\nУкажи, какая роль тебе подходит больше всего',
+                             reply_markup=keyboard)
         except Exception as e:
             print(e)
-            conn = sqlite3.connect("users.db")
-            cursor = conn.cursor()
-            insert = [message.from_user.id, '', '']
-            cursor.execute('INSERT INTO users VALUES (?, ?, ?)', insert)
-            conn.commit()
-            conn.close()
+
             keyboard = types.InlineKeyboardMarkup()
             key_buh = types.InlineKeyboardButton(text='Бухгалтер', callback_data='buh')
             keyboard.add(key_buh)
@@ -63,9 +58,6 @@ def start(message):
             bot.send_message(message.from_user.id,
                              'Привет, я бот MoreTechNews\n\nБот подберёт для тебя самые релевантные новости исходя из выбранной роли\n\nУкажи, какая роль тебе подходит больше всего',
                              reply_markup=keyboard)
-    if message.text == 'Другая':
-        pass
-
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -78,4 +70,33 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, 'Укажите название роли, которую хотите добавить:')
 
 
-bot.polling(none_stop=True, interval=0)
+def create_user_profile(id: int):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (`id`) VALUES (?)', [id])
+    conn.commit()
+    conn.close()
+    cursor.close()
+
+
+def init_db():
+    try:
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users')
+        cursor.close()
+        conn.close()
+        print('База данных найдена, начинаю работу!')
+    except Exception as e:
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            'CREATE TABLE users (id INTEGER PRIMARY KEY NOT NULL, role TEXT DEFAULT NULL, key_words TEXT DEFAULT NULL)')
+        conn.commit()
+        conn.close()
+        print('База данных не найдена, создаю и начинаю работу!')
+
+
+if __name__ == '__main__':
+    init_db()
+    bot.polling(none_stop=True, interval=0)
