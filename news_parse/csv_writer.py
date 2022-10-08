e4f0e3bbb422917e19dc74989eea514dda41e701
hteppl <hteppl.dev@gmail.com>
2022-10-08 03:24:59 +0300

    Update csv_writer.py

--- news_parse/csv_writer.py
+++ news_parse/csv_writer.py
@@ -5,7 +5,7 @@ def write(filename: str, article: str, body: str, date: str):
     try:
         with open(filename, mode='w', encoding='utf-8') as w_file:
             file_writer = csv.writer(w_file, delimiter=';', lineterminator='\r')
-            file_writer.writerow(['Article', 'Body', 'Date'])
+            file_writer.writerow(['article', 'body', 'date'])
     except Exception:
         print('Файл уже существует, начинаю сохранять данные:\n')
 
