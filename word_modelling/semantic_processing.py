6997892d5b3d484e10c937a66c58e741d6d7a5cb
hteppl <hteppl.dev@gmail.com>
2022-10-09 07:11:28 +0300

    Update semantic_processing.py

--- word_modelling/semantic_processing.py
+++ word_modelling/semantic_processing.py
@@ -5,14 +5,11 @@ from collections import Counter
 import itertools
 from statistics import median
 
-
-import pymorphy2
 import gensim.models
 import gensim.downloader
 import numpy as np
 import pandas as pd
 import spacy
-from nltk.corpus import stopwords
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sumy.parsers.plaintext import PlaintextParser
@@ -67,8 +64,6 @@ roles = {
 SPEECH_PARTS = ['NOUN', 'ADJ', 'VERB', 'ADV', 'PROPN']
 NLP = spacy.load("ru_core_news_lg")
 
-morph = pymorphy2.MorphAnalyzer()
-stopwords.words("russian")
 lex_rank_summarizer = LexRankSummarizer()
 
 
@@ -99,9 +94,9 @@ class SemanticProcessing:
         print("optimizing body finished")
 
     def optimize_article(self):
-        '''
+        """
         Оптимизация заголовка новости
-        '''
+        """
         print("optimizing articles")
 
         self.dataframe["Article_original"] = self.dataframe["Article"]
@@ -111,9 +106,9 @@ class SemanticProcessing:
         print("optimizing articles finished")
 
     def drop_similar(self):
-        '''
+        """
         Вычленение похожих новостей по процентному соотношению подобия
-        '''
+        """
         normalized_digests = self.dataframe["Digest"].apply(
             lambda x: NLP(" ".join(x)))
         combinations = list(itertools.combinations(normalized_digests, r=2))
@@ -129,11 +124,11 @@ class SemanticProcessing:
 
     @staticmethod
     def preprocess_text(word):
-        '''
+        """
         Лемматизация текста
         :param word:
         :return:
-        '''
+        """
         word = word.lower().replace("ё", "е")
         word = re.sub('((www\\.[^\\s]+)|(https?://[^\\s]+))', '', word)
         word = re.sub('@[^\\s]+', '', word)
@@ -144,12 +139,12 @@ class SemanticProcessing:
 
     @staticmethod
     def tf_list(article, n=10) -> list:
-        '''
+        """
         Частота появления термина.
         :param article:
         :param n:
         :return:
-        '''
+        """
         tf_idf_v = TfidfVectorizer(use_idf=True)
         tf_idf = tf_idf_v.fit_transform(article)
         result = pd.DataFrame(
@@ -161,25 +156,21 @@ class SemanticProcessing:
 
     @staticmethod
     def get_median_similarity_for_body(model, terms, role_keywords):
-        '''
+        """
         Медиана частоты появления слов в теле новости
         :param model:
         :param terms:
         :param role_keywords:
         :return:
-        '''
+        """
         result = []
 
         for article_word, keyword in itertools.product(terms, role_keywords):
             try:
                 result.append(
                     model.similarity(
-                        article_word +
-                        "_" +
-                        NLP(article_word)[0].pos_,
-                        keyword +
-                        "_" +
-                        NLP(keyword)[0].pos_))
+                        article_word + "_" + NLP(article_word)[0].pos_,
+                        keyword + "_" + NLP(keyword)[0].pos_))
             except KeyError as e:
                 result.append(0.0)
                 # print(e)
@@ -200,11 +191,11 @@ class SemanticProcessing:
 
     @staticmethod
     def find_digest(body):
-        '''
+        """
         Поиск дайджестов для формирования новостной рассылки
         :param body:
         :return:
-        '''
+        """
         pl_parser = PlaintextParser.from_string(body, Tokenizer('russian'))
         summary = lex_rank_summarizer(pl_parser.document, sentences_count=3)
         result = []
@@ -214,11 +205,11 @@ class SemanticProcessing:
 
     @staticmethod
     def find_grams(articles):
-        '''
+        """
         Заменяет словосочетания соответствующими n-граммами там, где это может быть нужно.
         :param articles:
         :return:
-        '''
+        """
         bigram = gensim.models.Phrases(articles)
         trigram = gensim.models.Phrases(bigram[articles])
         bigram_model = gensim.models.phrases.Phraser(bigram)
@@ -233,19 +224,19 @@ class SemanticProcessing:
         return result
 
     def find_trends(self):
-        '''
+        """
         Определяет тренды по частоте появления слов.
         :return:
-        '''
+        """
         self.optimize_article()
 
         div2 = int(len(self.dataframe) / 2)
         old_articles = self.dataframe["Article"].head(div2)
         new_articles = self.dataframe["Article"].tail(div2 - div2 % 2)
 
-        #c_old_articles = old_articles.apply(lambda x: pd.value_counts(x)).sum(axis=0).sort_values(ascending=False).apply(Counter)
-        #c_new_articles = new_articles.apply(lambda x: pd.value_counts(x)).sum(axis=0).sort_values(ascending=False).apply(Counter)
-        #print(c_old_articles, c_new_articles)
+        # c_old_articles = old_articles.apply(lambda x: pd.value_counts(x)).sum(axis=0).sort_values(ascending=False).apply(Counter)
+        # c_new_articles = new_articles.apply(lambda x: pd.value_counts(x)).sum(axis=0).sort_values(ascending=False).apply(Counter)
+        # print(c_old_articles, c_new_articles)
         c_old_articles = Counter(
             list(itertools.chain.from_iterable(old_articles)))
         c_new_articles = Counter(
@@ -267,7 +258,7 @@ class SemanticProcessing:
             if y_pred[i] != y_pred[i - 1]:
                 t_kw = dict(list(diff.items())[0:i])
                 break
-        print(t_kw)
+        return t_kw
 
 
 if __name__ == "__main__":
